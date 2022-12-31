0b313dfc0c1b16f7f2439059f250a7e24917643f
maxdepeuf <maximilien.bowen@univ-smb.fr>
2022-12-31 12:58:31 +0100

    new

---
+++ Chiro.js
@@ -0,0 +1,37 @@
+var a=0;
+function change_text(){
+      a=a+1;
+switch (a) {
+  case 1:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur1
+        document.getElementById("demo").innerHTML = "Test Genoux";
+    break;
+  case 2:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur2
+        document.getElementById("demo").innerHTML = "Test Nuque";
+    break;
+	  case 3:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur2
+        document.getElementById("demo").innerHTML = "Test Poitrine";
+    break;
+	  case 4:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur2
+        document.getElementById("demo").innerHTML = "Test coude";
+    break;
+	  case 5:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur2
+        document.getElementById("demo").innerHTML = "Test main";
+    break;
+	  case 6:
+    // Instructions à exécuter lorsque le résultat
+    // de l'expression correspond à valeur2
+        document.getElementById("demo").innerHTML = "Test Pied";
+    break;
+  }
+
+}
