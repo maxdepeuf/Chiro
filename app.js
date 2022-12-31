355a981345bb67b4dbdb1f98df73074daa4f36a0
maxdepeuf <maximilien.bowen@univ-smb.fr>
2022-12-31 13:35:32 +0100

    z

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ switch (a) {
     // Instructions à exécuter lorsque le résultat
     // de l'expression correspond à valeur1
         document.getElementById("demo").innerHTML = "Test Genoux";
+		document.getElementById("myImageId").src="hip.png";
     break;
   case 2:
     // Instructions à exécuter lorsque le résultat
     // de l'expression correspond à valeur2
         document.getElementById("demo").innerHTML = "Test Nuque";
+		document.getElementById("myImageId").src="neck.png";
     break;
 	  case 3:
     // Instructions à exécuter lorsque le résultat
     // de l'expression correspond à valeur2
         document.getElementById("demo").innerHTML = "Test Poitrine";
+		document.getElementById("myImageId").src="chest.png";
     break;
 	  case 4:
     // Instructions à exécuter lorsque le résultat
